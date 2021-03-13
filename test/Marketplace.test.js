404ce60b7a2729faf9a6a8699287016d05d91775
JamesChan <djkskqyr3@hotmail.com>
2021-03-13 19:36:15 +0800

    create product and test

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,8 +1,10 @@
-const { assert } = require("chai")
-
 const Marketplace = artifacts.require('./Marketplace.sol')
 
-contract('Marketplace', (accounts) => {
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace
 
     before(async () => {
@@ -21,6 +23,34 @@ contract('Marketplace', (accounts) => {
         it('has a name', async () => {
             const name = await marketplace.name()
             assert.equal(name, 'Dapp University Marketplace')
+        })
+    })
+
+    describe('products', async () => {
+        
+        let result, productCount;
+
+        before(async () => {
+            result = await marketplace.createProduct('iPhoneX', web3.utils.toWei('1', 'Ether'), { from: seller })
+            productCount = await marketplace.productCount()
+        })
+
+        it('create products', async () => {     
+            
+            // SUCCESS
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'iPhoneX', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, 'purchased is correct')
+
+            // FAILURE : Product must have a name
+            await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected
+            // FAILURE : Product must have a price
+            await await marketplace.createProduct('iPhoneX', 0, { from: seller }).should.be.rejected
+            
         })
     })
 })
